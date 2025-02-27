be70b5bb69a24ad4b12d5f2f069f13dade4e0ac3
chinasa056 <chinasaacha05@gmail.com>
2025-02-27 20:54:55 +0100

    rectified the admin  routes

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,12 +1,22 @@
-const { getAllTeachers, getAllStudents, updateTeacher, updateStudent, deleteStudent, deleteTeacher, registerAdmin, changePassword, forgotPassword, removeTeacherAdminAccess, getStudentByStack, verifyAdmin } = require("../controllers/adminController");
+const { getAllTeachers, getAllStudents, updateTeacher, updateStudent, deleteStudent, deleteTeacher, registerAdmin, changePassword, forgotPassword, removeTeacherAdminAccess, getStudentByStack, verifyAdmin, loginAdmin, resetPassword } = require("../controllers/adminController");
 const { superAdminAuth, authenticate, adminAuth } = require("../middleware/authentication");
 
 const router = require("express").Router();
 
-router.get
-("/admin_verify/:token", verifyAdmin)
+router.get("/admin_verify/:token", verifyAdmin)
 
 router.post("/register/", registerAdmin)
+
+router.post("/login", loginAdmin)
+
+router.post("/forgot_password", forgotPassword);
+
+router.post("/reset_password", resetPassword)
+
+router.post("/change_password", changePassword)
+
+
+
 
 router.get("/all_teachers", superAdminAuth, getAllTeachers)
 
